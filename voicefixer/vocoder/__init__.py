ae130b8f8834d87d12a995581bf7393959014d93
mrfakename <me@mrfake.name>
2023-11-15 18:31:32 -0800

    Change model URL

--- voicefixer/vocoder/__init__.py
+++ voicefixer/vocoder/__init__.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""
-@File    :   __init__.py.py    
-@Contact :   haoheliu@gmail.com
-@License :   (C)Copyright 2020-2100
-
-@Modify Time      @Author    @Version    @Desciption
-------------      -------    --------    -----------
-9/14/21 1:00 AM   Haohe Liu      1.0         None
-"""
-
 import os
 from voicefixer.vocoder.config import Config
 import urllib.request
@@ -18,7 +6,7 @@ if not os.path.exists(Config.ckpt):
     os.makedirs(os.path.dirname(Config.ckpt), exist_ok=True)
     print("Downloading the weight of neural vocoder: TFGAN")
     urllib.request.urlretrieve(
-        "https://zenodo.org/record/5469951/files/model.ckpt-1490000_trimed.pt?download=1",
+        "https://huggingface.co/voicefixer/voicefixer/resolve/main/vocoder/model.ckpt-1490000_trimed.pt?download=true",
         Config.ckpt,
     )
     print(
@@ -26,5 +14,3 @@ if not os.path.exists(Config.ckpt):
             Config.ckpt, os.path.getsize(Config.ckpt)
         )
     )
-    # cmd = "wget https://zenodo.org/record/5469951/files/model.ckpt-1490000_trimed.pt?download=1 -O " + Config.ckpt
-    # os.system(cmd)
